07130181e1185365b1422731a923f90da020eda9
vbox@mfranzil-s16 <vbox@rising.fbk.eu>
2023-05-18 08:35:17 +0200

    Updated test, SA, and gitignore

--- kfiles/limited/fa.sh
+++ kfiles/limited/fa.sh
@@ -12,4 +12,8 @@ kubectl apply -f 5gb-3.yaml
 sleep 2
 kubectl delete -f 5gb-3.yaml --force --grace-period=0
 sleep 2
+kubectl apply -f 5gb-4.yaml
+sleep 2
+kubectl delete -f 5gb-4.yaml --force --grace-period=0
+sleep 2
 kubectl apply -f httpd.yaml
