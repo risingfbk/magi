725a70e8f6a730ec0e325afda88bbc1240430094
Matteo Franzil <matteofranzil@gmail.com>
2023-06-07 11:26:34 +0200

    Making progress

--- magi_system/init_master.sh
+++ magi_system/init_master.sh
@@ -6,4 +6,4 @@ if [[ "$UID" -ne 0 ]]; then
   exit 1
 fi
 
-sudo tail -F /var/log/kubernetes/audit/audit.log | grep "/pods" | jq 'select(.verb == "delete" or .verb == "create") | {requestURI: .requestURI, verb: .verb, stage: .stage, imageRequest: .responseObject.spec.containers[0].image, targetNode: .requestObject.target.name, respondingNode: .responseObject.spec.nodeName}'
+tail -F /var/log/kubernetes/audit/audit.log | grep "/pods" | jq 'select(.verb == "delete" or .verb == "create") | {requestURI: .requestURI, verb: .verb, stage: .stage, imageRequest: .responseObject.spec.containers[0].image, targetNode: .requestObject.target.name, respondingNode: .responseObject.spec.nodeName}'
