1876f9bc9d874d9710951bd551611af787d10855
Matteo Franzil <matteofranzil@gmail.com>
2023-06-28 11:58:13 +0200

    Fresh new graphs straight out of the fridge

--- plot_data.py
+++ plot_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.dates as mdates
 import os
+import datetime
 
 avail = os.listdir('results/')
 directory = input(f"Which directory do you want to plot? {avail} ")
@@ -40,10 +41,15 @@ MAPPINGS = {
         "ylabel": "Network Transmit (MB/s)",
     },
     'worker2cpu': {
-        "title": "Worker2 CPU Usage by mode",
+        "title": "Worker CPU usage by mode",
         "ylabel": "CPU Usage (%)",
     },
+    'disk_w+network_r': {
+        "title": "Disk writes and network receives",
+        "ylabel": "Throughput (MB/s)",
+    }
 }
+
 
 def plot_normal(df: pd.DataFrame, plot: str):
     df.rename(
@@ -60,6 +66,9 @@ def plot_normal(df: pd.DataFrame, plot: str):
         inplace=True
     )
 
+    # delete all columns != worker2
+    # df.drop(columns=df.columns[~df.columns.str.contains("Worker2|Time", regex=True)], inplace=True)
+
     if plot in ("disk_r", "disk_w", "network_r", "network_w"):
         df['Master'] = df['Master'] / 1024 / 1024
         df['Worker1'] = df['Worker1'] / 1024 / 1024
@@ -68,9 +77,15 @@ def plot_normal(df: pd.DataFrame, plot: str):
 
     # df = df[(df['Time'] >= '2023-05-14 19:23:00') & (df['Time'] <= '2023-05-14 19:45:00')]
 
-    ax = df.plot(x='Time', y='Master')
-    df.plot(x='Time', y='Worker1', ax=ax)
-    df.plot(x='Time', y='Worker2', ax=ax)
+    df.rename(
+        columns={
+            'Worker2': 'Worker'
+        },
+        inplace=True
+    )
+    ax = df.plot(x='Time', y='Worker', figsize=(6, 4))
+    # df.plot(x='Time', y='Worker1', ax=ax)
+    # df.plot(x='Time', y='Worker2', ax=ax)
     # df.plot(x='Time', y='Registry', ax=ax)
 
     ticks = []
@@ -78,27 +93,89 @@ def plot_normal(df: pd.DataFrame, plot: str):
     mmax = df['Time'].max() - (df['Time'].max() % 60) + 60
     for i in range(0, mmax, 60):
         ticks.append(i)
-        labels.append(str(i // 60))
+        labels.append(datetime.datetime.utcfromtimestamp(i).strftime('%M:%S'))
     ax.set_xticks(ticks)
     ax.set_xticklabels(labels)
-    
+    ax.set_xlim([-0.01, mmax - 60 + 0.01])
+
     # ax.xaxis.set_major_locator(mdates.SecondLocator(interval=60))
     # ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
 
     print(df.head())
 
-    plt.gcf().autofmt_xdate()
+    # plt.gcf().autofmt_xdate()
 
     # Set the labels
-    ax.set_xlabel('Time (min)')
+    ax.set_xlabel('Time')
     ax.set_ylabel(MAPPINGS[plot]['ylabel'])
     ax.set_title(MAPPINGS[plot]['title'])
-    
+
     # Save the figure
+    plt.tight_layout()
+    plt.savefig(PLOT_DIR + '/' + plot + '.png')
+
+
+def plot_disk_network(df_disk: pd.DataFrame, df_network: pd.DataFrame, plot: str):
+    for df in (df_disk, df_network):
+        df["Time"].apply(pd.to_timedelta, unit='s')
+        df["Time"] = df["Time"] - df["Time"].iloc[0]
+        df.rename(
+            columns={
+                '{instance="192.168.221.10:9100"}': 'Master',
+                '{instance="192.168.221.11:9100"}': 'Worker1',
+                '{instance="192.168.221.12:9100"}': 'Worker2',
+                '{instance="10.231.0.208:9100"}': 'Registry',
+                '{instance="192.168.221.10:9100",job="prometheus"}': 'Master',
+                '{instance="192.168.221.11:9100",job="prometheus"}': 'Worker1',
+                '{instance="192.168.221.12:9100",job="prometheus"}': 'Worker2',
+                '{instance="10.231.0.208:9100",job="prometheus"}': 'Registry',
+            },
+            inplace=True
+        )
+    # Merge the two df by creating a new df (time, disk, network) of only worker2
+    df = pd.merge(df_disk, df_network, on='Time')
+    df = df[['Time', 'Worker2_x', 'Worker2_y']]
+    df.rename(
+        columns={
+            'Worker2_x': 'Disk',
+            'Worker2_y': 'Network',
+        },
+        inplace=True
+    )
+
+    df['Disk'] = df['Disk'] / 1024 / 1024
+    df['Network'] = df['Network'] / 1024 / 1024
+
+    ax = df.plot(x='Time', y='Disk', figsize=(6, 4))
+    df.plot(x='Time', y='Network', ax=ax)
+
+    # On the left put the ticks for the disk, on the right the ticks for the network
+    ax.set_xlim([-0.01, df['Time'].max() - (df['Time'].max() % 60) + 60 + 0.01])
+
+    ticks = []
+    labels = []
+    mmax = df['Time'].max() - (df['Time'].max() % 60) + 60
+    for i in range(0, mmax, 60):
+        ticks.append(i)
+        labels.append(datetime.datetime.utcfromtimestamp(i).strftime('%M:%S'))
+    ax.set_xticks(ticks)
+    ax.set_xticklabels(labels)
+    ax.set_xlim([-0.01, mmax - 60 + 0.01])
+
+    ax.set_xlabel('Time')
+    ax.set_ylabel(MAPPINGS[plot]['ylabel'])
+    ax.set_title(MAPPINGS[plot]['title'])
+
+    print(df.head())
+
+    plt.grid(axis='y')
+    plt.tight_layout()
     plt.savefig(PLOT_DIR + '/' + plot + '.png')
 
 
 def plot_worker2cpu(df: pd.DataFrame, plot: str):
+    df["Time"].apply(pd.to_timedelta, unit='s')
+    df["Time"] = df["Time"] - df["Time"].iloc[0]
     df.rename(
         columns={
             '{mode="idle"}': 'Idle',
@@ -114,7 +191,7 @@ def plot_worker2cpu(df: pd.DataFrame, plot: str):
     )
 
     # compress the dataframe, taking the average of each 60s interval
-    df = df.groupby(np.arange(len(df))//60).mean()
+    # df = df.groupby(np.arange(len(df))//60).mean()
     # set the time column to 0, 1, 2, ...
     df['Time'] = df.index
 
@@ -127,31 +204,34 @@ def plot_worker2cpu(df: pd.DataFrame, plot: str):
         for col in df.columns[1:]:
             df.at[i, col] = df.at[i, col] / s * 100
 
-    print(df.head())
-
     # stop sns's white border on the bars
 
     # use a palette that colors the bars in the same color as the lines
     # plot a barplot. in each row, stack all the values that are at time t, t+60
-    ax = df.plot.bar(x='Time', y=df.columns[1:], stacked=True, edgecolor='none')
+    ax = df.plot.area(x='Time', y=df.columns[1:4], stacked=True, figsize=(6, 4))
 
     ticks = []
     labels = []
-    for i in range(0, df['Time'].max() + 1):
+    mmax = df['Time'].max() - (df['Time'].max() % 60) + 60
+    for i in range(0, mmax, 60):
         ticks.append(i)
-        labels.append(str(i))
+        labels.append(datetime.datetime.utcfromtimestamp(i).strftime('%M:%S'))
     ax.set_xticks(ticks)
     ax.set_xticklabels(labels)
+    ax.set_xlim([-0.01, mmax - 60 + 0.01])
+    ax.set_ylim(0, 100)
 
     # Set the labels
-    ax.set_xlabel('Time (min)')
+    ax.set_xlabel('Time')
     ax.set_ylabel(MAPPINGS[plot]['ylabel'])
     ax.set_title(MAPPINGS[plot]['title'])
 
-    ax.set_ylim(0, 100)
+    print(df.head())
 
     # Save the figure
+    plt.tight_layout()
     plt.savefig(PLOT_DIR + '/' + plot + '.png')
+
 
 
 def main():
@@ -159,29 +239,24 @@ def main():
     plots = os.listdir(DATA_DIR)
     os.makedirs(PLOT_DIR, exist_ok=True)
 
+    plots.append('disk_w+network_r')
+
     for plot in plots:
-        df = pd.read_csv(DATA_DIR + '/' + plot, sep=';')
-        # header
-        # Plot the data
-        sns.set(rc={'figure.figsize': (6, 6), 'figure.dpi': 300, 'savefig.dpi': 300}, font_scale=.9)
+        sns.set(rc={'figure.figsize': (6, 4), 'figure.dpi': 300, 'savefig.dpi': 300}, font_scale=.9)
 
         sns.set_style('whitegrid')
         sns.set_context('paper')
         sns.set_palette('colorblind')
 
-        # reduce margins to a minimum
+        if plot == "worker2cpu":
+            df = pd.read_csv(DATA_DIR + '/' + plot, sep=';')
 
-        # convert the time to datetime; time is like 1684088862
-        df["Time"].apply(pd.to_timedelta, unit='s')
-        df["Time"] = df["Time"] - df["Time"].iloc[0]
-        #df['Time'] = pd.to_datetime(df['Time'], unit='s')
-        # df["round_time"].apply(pd.to_timedelta, unit='s',
-        # df = df.groupby(by=[pd.Grouper(freq='1S', key='Time')]).mean().reset_index()
-
-        if plot != "worker2cpu":
-            plot_normal(df, plot)
-        else:
             plot_worker2cpu(df, plot)
+        elif plot == "disk_w+network_r":
+            df_disk = pd.read_csv(DATA_DIR + '/' + 'disk_w', sep=';')
+            df_network = pd.read_csv(DATA_DIR + '/' + 'network_r', sep=';')
+
+            plot_disk_network(df_disk=df_disk, df_network=df_network, plot=plot)
 
 
 if __name__ == "__main__":
