8e7576a11327abffb2750a45ee52c6e367188d66
Matteo Franzil <matteofranzil@gmail.com>
2023-07-03 08:53:20 +0200

    Preparing for new exps

---
+++ kfiles/limited/exp_1+2/2_upto7.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+kubectl apply -f 5gb-1.yaml
+sleep 2
+kubectl delete -f 5gb-1.yaml --force --grace-period=0
+sleep 2
+kubectl apply -f 5gb-2.yaml
+sleep 2
+kubectl delete -f 5gb-2.yaml --force --grace-period=0
+sleep 2
+kubectl apply -f 5gb-3.yaml
+sleep 2
+kubectl delete -f 5gb-3.yaml --force --grace-period=0
+sleep 2
+kubectl apply -f 5gb-4.yaml
+sleep 2
+kubectl delete -f 5gb-4.yaml --force --grace-period=0
+sleep 2
+kubectl apply -f httpd.yaml
