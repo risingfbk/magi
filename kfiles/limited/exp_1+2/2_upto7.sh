1c52778ad20c546e1072e06351132a2527320761
Matteo Franzil <matteofranzil@gmail.com>
2023-07-03 08:56:58 +0200

    Finalized experiment

--- kfiles/limited/exp_1+2/2_upto7.sh
+++ kfiles/limited/exp_1+2/2_upto7.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-kubectl apply -f 5gb-1.yaml
-sleep 2
-kubectl delete -f 5gb-1.yaml --force --grace-period=0
-sleep 2
-kubectl apply -f 5gb-2.yaml
-sleep 2
-kubectl delete -f 5gb-2.yaml --force --grace-period=0
-sleep 2
-kubectl apply -f 5gb-3.yaml
-sleep 2
-kubectl delete -f 5gb-3.yaml --force --grace-period=0
-sleep 2
-kubectl apply -f 5gb-4.yaml
-sleep 2
-kubectl delete -f 5gb-4.yaml --force --grace-period=0
-sleep 2
-kubectl apply -f httpd.yaml
+# Randomize the order of the pods
+for i in $(shuf <(ls 5gb-*)); do
+    echo "Applying $i"
+    kubectl apply -f $i
+    sleep 2
+    kubectl delete -f $i --force --grace-period=0
+done
+kubectl apply -f ../httpd.yaml
