2bcd8bef2b14953e87596e774b02f8a7b5fc1792
Matteo Franzil <matteofranzil@gmail.com>
2023-07-28 11:32:15 +0200

    Renamed experiment folders Generation of experiments automatically

---
+++ kfiles/limited/exp_3_randomgb/3_random_upto7.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Randomize the order of the pods
+for i in $(seq 1 | shuf); do
+    echo "Applying $i"
+    name="randommb-$i.yml"
+cat << EOF > "$name"
+apiVersion: v1
+kind: Pod
+metadata:
+  name: randomgb-1
+  namespace: limited
+spec:
+  containers:
+  - name: randomgb
+    image: registry-10-231-0-208.nip.io/mfranzil/randomgb:$i
+EOF
+    kubectl apply -f "$name"
+    sleep 2
+    kubectl delete -f "$name" --force --grace-period=0
+done
