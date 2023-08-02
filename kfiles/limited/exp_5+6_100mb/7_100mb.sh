a64de037f9844cb15d2873be5a062f1b2448cac9
Matteo Franzil <matteofranzil@gmail.com>
2023-08-02 16:45:45 +0200

    Updated stxy position, new iperf3 dockerfile, new exp #7

---
+++ kfiles/limited/exp_5+6_100mb/7_100mb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ -z $REGISTRY_IP_DOMAIN ]]; then
+    echo "The domain with its port of the registry is missing. E.g. registry.example.com:8080. Please add it to the variable \$REGISTRY_IP_DOMAIN."
+    exit
+fi
+
+# Randomize the order of the pods
+    for i in $(seq 1 40 | shuf); do
+    echo "Applying $i"
+    name="100mb-$i-generated.yml"
+cat << EOF > "$name"
+apiVersion: v1
+kind: Pod
+metadata:
+  name: 100mb-$i
+  namespace: limited
+spec:
+  containers:
+  - name: 100mb
+    image: $REGISTRY_IP_DOMAIN/mfranzil/100mb:$i
+EOF
+    kubectl apply -f "$name"
+    sleep 5
+    kubectl delete -f "$name" --force --grace-period=0
+done
