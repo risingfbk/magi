5e9419a032c801f722a2c8adeaf69258b7cbcd5a
Matteo Franzil <matteofranzil@gmail.com>
2023-08-31 11:00:42 +0200

    More modifications to test, moved metrics file

--- kfiles/limited/exp_b_magi/deploy.sh
+++ kfiles/limited/exp_b_magi/deploy.sh
@@ -6,27 +6,41 @@ kubectl delete all -n exp-b-magi --all
 # Create a new namespace
 kubectl create namespace exp-b-magi
 
+echo "Start time:"
+date --iso-8601=seconds
+
 # Launch a few legit services from Dockerhub, with our attack intertwined
 kubectl run --image sagemathinc/cocalc:d97b1277eb1c cocalc -n exp-b-magi &
+sleep 20
 
 if [[ "$1" == "--attack" ]]; then
-    ../exp_3_randomgb/3_random_upto7.sh &
+    cd ../exp_3_randomgb
+    ./3_random_upto7.sh &
+    cd -
 fi
-sleep 2
+sleep 30
 
 # Create a fake secret containing BS credentials, because why not
-kubectl create secret docker-registry regcred --docker-server=http://fake.server.com:5000 --docker-username=test --docker-password=test -n exp-b-magi &
-sleep 2
+# kubectl create secret docker-registry regcred --docker-server=http://fake.server.com:5000 --docker-username=test --docker-password=test -n exp-b-magi &
+# sleep 2
 
 kubectl run --image jupyter/scipy-notebook adder -n exp-b-magi &
 sleep 20
 
-kubectl run --image $REGISTRY_IP_DOMAIN/mfranzil/obese-httpd:50 obese -n exp-b-magi &
+kubectl run --image nginx:mainline-alpine3.18-perl nginx -n exp-b-magi &
 sleep 20
 
-kubectl delete pod/adder -n exp-b-magi &
-
 kubectl apply -n exp-b-magi -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/main/release/kubernetes-manifests.yaml &
-sleep 14
+sleep 20
 
-# kubectl delete all -n exp-b-magi --all
+echo "Times for adder"
+kubectl get pod/adder -n exp-b-magi -o yaml | yq '.status.conditions'
+kubectl delete pod/adder -n exp-b-magi
+date --iso-8601=seconds
+
+echo "When it's finished, run the following commands"
+echo "k get pod -n exp-b-magi cocalc -o yaml | yq '.status.conditions'"
+echo "k get pod -n exp-b-magi nginx -o yaml | yq '.status.conditions'"
+echo "k get pod -n exp-b-magi frontend -o yaml | yq '.status.conditions'"
+echo "k get pod -n exp-b-magi adservice -o yaml | yq '.status.conditions'"
+echo "k delete all -n exp-b-magi --all"
