3402b44ea3602197593dabe27541ad0ccd571da2
Matteo Franzil <matteofranzil@gmail.com>
2023-09-13 13:45:48 +0200

    data update and increased res to 15s for gantt

--- gantt_gc.py
+++ gantt_gc.py
@@ -8,15 +8,11 @@ import math
 
 images = list(range(1, 8))
 t = [sum(range(1, i)) for i in images]
-duration = copy.deepcopy(images)
+t = [0.0, 3.0, 8.0, 15.0, 25.0, 35.0, 49.0, 65.0]
+duration = [t[i+1] - t[i] for i in range(len(t) - 1)]
 images = [str(i) for i in images]
 
-y = [67.409, 68.313, 69.189, 70.421, 71.547, 73.286, 74.853, 76.262, 77.578,
-     78.645, 79.446, 80.130, 80.765, 81.414, 82.009,
-     82.519, 82.921, 83.324, 83.677, 83.924, 84.149,
-     84.359, 84.559, 84.759, 84.948, 85.137, 70.637,
-     71.288, 72.513, 73.430, 73.430, 73.430, 73.430,
-     73.430, 73.430]
+y = [29, 30, 31, 32, 33, 34, 35, 36, 37, 39, 40, 41, 42, 43, 44, 45, 46, 47, 49, 50, 51, 52, 53, 54, 55, 56, 57, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 85, 61, 63, 64, 66, 67, 68, 68, 69, 70, 71, 72, 73, 74, 75, 75]
 
 sns.set_context('paper')
 sns.set(rc={'figure.figsize': (12, 5), 'figure.dpi': 300, 'savefig.dpi': 300}, font_scale=1.8)
@@ -37,21 +33,24 @@ fig.subplots_adjust(right=0.75)
 
 for i in range(len(images)):
     ax1.barh(i, duration[i], alpha=1, height=0.3, align='center', edgecolor='white', left=t[i])
-    if i != len(images) - 1:
-        ax1.barh(i, sum(duration[i:]) - duration[-1] - i + 3, alpha=0.3, height=0.3, align='center', color='black', left=t[i] + duration[i], linestyle=":", linewidth=1, edgecolor="black", hatch='//')
+    if i in (0, 1, 2, 3):
+        ll = 52 - (t[i] + duration[i])
+    else:
+        ll = sum(duration[i:])
+    ax1.barh(i, ll, alpha=0.3, height=0.3, align='center', color='black', left=t[i] + duration[i], linestyle=":", linewidth=1, edgecolor="black", hatch='//')
 
-ax2.plot(range(0, 35, 1), y, alpha=0, linewidth=0)
-ax2.fill_between(range(0, 35, 1), y, ['0'] * len(y), alpha=0.2, color=sns.color_palette()[0])
-ax2.set_ylim([65, 86])
-ax2.set_yticks(range(65, 90, 5))
-ax2.set_yticklabels(range(65, 90, 5))
+ax2.plot(range(0, 67, 1), y, alpha=0, linewidth=0)
+ax2.fill_between(range(0, 67, 1), y, ['0'] * len(y), alpha=0.2, color=sns.color_palette()[0])
+ax2.set_ylim([28, 86])
+ax2.set_yticks(range(25, 90, 10))
+ax2.set_yticklabels(range(25, 90, 10))
 
 ax.set_yticks(np.arange(len(images)))
 ax.set_yticklabels(images)
-ax.set_xlim(0, 30)
+ax.set_xlim(0, 67)
 ax.set_ylim(-0.5, 6.5)
 
-ax.vlines([25], -1, 7, colors="black", linestyles=":", linewidth=1.5)
+ax.vlines([52], -1, 7, colors="black", linestyles=":", linewidth=1.5)
 
 # ax.set_xticks(range(0, 35, 2))
 ax.set_xlabel('Time (m)')
