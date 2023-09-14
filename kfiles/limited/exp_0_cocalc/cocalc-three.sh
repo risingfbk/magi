4fa1a52c7b9a6a72e1acf8f40ab3cf9ba009cee6
Matteo Franzil <matteofranzil@gmail.com>
2023-09-14 09:35:53 +0200

    Important

--- kfiles/limited/exp_0_cocalc/cocalc-three.sh
+++ kfiles/limited/exp_0_cocalc/cocalc-three.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-kubectl run --image sagemathinc/cocalc:d97b1277eb1c test1
+kubectl run -n limited --image sagemathinc/cocalc:d97b1277eb1c test1
 sleep 10
-kubectl delete --force pod/test1
-kubectl run --image sagemathinc/cocalc:fcddf25ba2aa test1
+kubectl -n limited delete --force pod/test1
+kubectl run -n limited --image sagemathinc/cocalc:fcddf25ba2aa test1
 sleep 10
-kubectl delete --force pod/test1
-kubectl run --image sagemathinc/cocalc:be98b03ccae8 test1
+kubectl -n limited delete --force pod/test1
+kubectl run -n limited --image sagemathinc/cocalc:be98b03ccae8 test1
 sleep 10
-kubectl delete --force pod/test1
+kubectl -n limited delete --force pod/test1
+kubectl run -n limited --image nginx:latest test2
---
+++ kfiles/limited/exp_g_gc/attack.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ -z $REGISTRY_IP_DOMAIN ]]; then
+    echo "The domain with its port of the registry is missing. E.g. registry.example.com:8080. Please add it to the variable \$REGISTRY_IP_DOMAIN."
+    exit
+fi
+
+# Randomize the order of the pods
+for i in $(seq 1 7); do
+    echo "Applying $i"
+    name="randomgb-$i-generated.yml"
+cat << EOF > "$name"
+apiVersion: v1
+kind: Pod
+metadata:
+  name: randomgb-$i
+  namespace: limited
+spec:
+  containers:
+  - name: randomgb
+    image: $REGISTRY_IP_DOMAIN/mfranzil/randomgb:$i
+EOF
+    kubectl apply -f "$name"
+    sleep 2
+    kubectl delete -f "$name"  ## --force --grace-period=0
+done
