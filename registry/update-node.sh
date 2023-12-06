55b2e4bfa3f5883f5728cbfc54fbe8aecf99d262
Matteo Franzil <mfranzil@fbk.eu>
2023-12-06 14:39:53 +0100

    Add script to update node certificates remotely

---
+++ registry/update-node.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    echo "Need the target host name"
+    exit
+fi
+
+scp certs/$REGISTRY_IP_DOMAIN.crt $1
+ssh $1 "sudo cp ~/$REGISTRY_IP_DOMAIN.crt /usr/local/share/ca-certificates/ && sudo update-ca-certificates && sudo systemctl restart containerd"
